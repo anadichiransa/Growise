87e7b65a48d2644c65475e68820977b6e6e8d492
Anadi Chiransa <anadipiyamini@gmail.com>
2026-02-26 16:33:15 +0530

    Added static function to blend colours

--- frontend/lib/core/constants/colors.dart
+++ frontend/lib/core/constants/colors.dart
@@ -7,4 +7,9 @@ class AppColours {
   static const Color deepMagenta = Color(0xFF4F1C51);
   static const Color mintGreen = Color(0xFF13ECA4);
   static const Color white = Color(0xFFFFFFFF);
+
+  //White mixed colours
+  static Color getWhiteMix(Color color, double opacity) {
+    return Color.alphaBlend(Colors.white.withOpacity(opacity), color);
+  }
 }
