ef4f3efe7c2d73a542d04d20f3facbbe9e815c63
Anadi Chiransa <anadipiyamini@gmail.com>
2026-02-26 16:29:06 +0530

    Implemented the class AppColours and added the brand colours

---
+++ frontend/lib/core/constants/colors.dart
@@ -0,0 +1,10 @@
+import "package:flutter/material.dart";
+
+class AppColours {
+  //brand colours
+  static const Color background = Color(0xFF210F37);
+  static const Color primaryGold = Color(0xFFFFC44D);
+  static const Color deepMagenta = Color(0xFF4F1C51);
+  static const Color mintGreen = Color(0xFF13ECA4);
+  static const Color white = Color(0xFFFFFFFF);
+}
