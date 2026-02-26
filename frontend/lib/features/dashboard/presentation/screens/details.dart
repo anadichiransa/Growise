1e6aac113706ee7140b6769670a21d4f72cd5f77
Ackshayan <ackshisusi@gmail.com>
2026-02-26 13:42:27 +0530

    Details : add GroWiseColors + imports

---
+++ frontend/lib/features/dashboard/presentation/screens/details.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class GroWiseColors {
+  GroWiseColors._();
+  
+  static const Color backgroundDark = Color(0xFF1A0F2E);
+  static const Color backgroundPurple = Color(0xFF2D1B4E);
+  static const Color accentGold = Color(0xFFFFB84D);
+  static const Color primaryPurple = Color(0xFF9B6FD9);
+  static const Color textWhite = Color(0xFFFFFFFF);
+  static const Color textGray = Color(0xFFB8B8D8);
+  static const Color cardPurple1 = Color(0xFF7D5BA6);
+  static const Color cardPurple2 = Color(0xFF6B4A93);
+  static const Color cardPurple3 = Color(0xFF5A3980);
+}
