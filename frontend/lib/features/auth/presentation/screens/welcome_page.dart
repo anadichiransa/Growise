4ec9b6b1359c3e25549be6205d4a7a66240e2d90
Anadi Chiransa <anadipiyamini@gmail.com>
2026-03-03 15:13:29 +0530

    Set up file structure and scaffold background

---
+++ frontend/lib/features/auth/presentation/screens/welcome_page.dart
@@ -0,0 +1,13 @@
+import "package:flutter/material.dart";
+
+class OnboradingScreen extends StatelessWidget {
+  const OnboradingScreen({super.key});
+
+  @override
+  Widget build(BuildContext) {
+    return Scaffold(
+      backgroundColor: const Color(0xFF210F37),
+      body: Container(),
+    );
+  }
+}
