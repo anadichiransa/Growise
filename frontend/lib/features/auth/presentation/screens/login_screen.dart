717384df08e91a2b011b749bcaac8edcf134a96b
Anadi Chiransa <anadipiyamini@gmail.com>
2026-03-03 23:42:55 +0530

    Initialize LoginScreen and text controllers

---
+++ frontend/lib/features/auth/presentation/screens/login_screen.dart
@@ -0,0 +1,19 @@
+import "package:flutter/material.dart";
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  bool _isPasswordVisible = false;
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Container());
+  }
+}
