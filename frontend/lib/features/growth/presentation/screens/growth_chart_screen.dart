e2a866393fa1527de3eb969cd750c77a7dc0cb0d
AsindU_YasitH <asinduyasithudara.2004@gmail.com>
2026-03-05 14:45:59 +0530

    Add growth_chart_screen.dart with imports
    Create a new growth_chart_screen.dart file and add imports required for implementing the growth chart UI: Flutter, fl_chart, status banner and bottom navigation components, growth controller and model, and screens for adding/saved measurements. The file currently contains only import statements as scaffolding for the screen implementation.

---
+++ frontend/lib/features/growth/presentation/screens/growth_chart_screen.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:fl_chart/fl_chart.dart';
+import '../../components/common/status_banner.dart';
+import '../../controllers/growth_controller.dart';
+import '../../models/growth_record_model.dart';
+import '../../components/common/bottom_nav.dart';
+import 'add_measurement_screen.dart';
+import 'saved_measurements_screen.dart';
