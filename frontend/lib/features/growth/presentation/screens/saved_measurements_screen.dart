1e0e0c428a6a559bc7f662e6dddd8f65b1463c62
AsindU_YasitH <asinduyasithudara.2004@gmail.com>
2026-03-05 14:49:32 +0530

    Add saved_measurements_screen.dart
    Create a new placeholder screen file saved_measurements_screen.dart. The file imports Flutter Material and project dependencies (growth_controller, growth_record_model, and common bottom_nav) and prepares the module for implementing the saved measurements UI; no widget implementation has been added yet.

---
+++ frontend/lib/features/growth/presentation/screens/saved_measurements_screen.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+import '../../controllers/growth_controller.dart';
+import '../../models/growth_record_model.dart';
+import '../../components/common/bottom_nav.dart';
