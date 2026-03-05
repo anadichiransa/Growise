39febdf5e9e87d017e2718028c99658a2c4393a1
AsindU_YasitH <asinduyasithudara.2004@gmail.com>
2026-03-05 14:50:09 +0530

    Add SavedMeasurementsScreen StatefulWidget
    Introduce SavedMeasurementsScreen in saved_measurements_screen.dart as a StatefulWidget with required constructor parameters: childId, childName, gender, and dateOfBirth. Prepares the screen to receive child context for displaying saved growth measurements (UI/logic to be implemented).

--- frontend/lib/features/growth/presentation/screens/saved_measurements_screen.dart
+++ frontend/lib/features/growth/presentation/screens/saved_measurements_screen.dart
@@ -2,3 +2,17 @@ import 'package:flutter/material.dart';
 import '../../controllers/growth_controller.dart';
 import '../../models/growth_record_model.dart';
 import '../../components/common/bottom_nav.dart';
+
+class SavedMeasurementsScreen extends StatefulWidget {
+  final String childId;
+  final String childName;
+  final String gender;
+  final DateTime dateOfBirth;
+
+  const SavedMeasurementsScreen({
+    super.key,
+    required this.childId,
+    required this.childName,
+    required this.gender,
+    required this.dateOfBirth,
+  });
