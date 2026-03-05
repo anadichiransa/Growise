4b303bf06927d062432010ad06bdcf0628a5f977
AsindU_YasitH <asinduyasithudara.2004@gmail.com>
2026-03-05 14:43:39 +0530

    Add AddMeasurementScreen widget
    Introduce AddMeasurementScreen StatefulWidget that accepts required parameters: childId, childName, gender, and dateOfBirth. This change scaffolds a dedicated screen for adding growth measurements and exposes the child's context via constructor fields; UI and controller logic will be implemented in subsequent changes.

--- frontend/lib/features/growth/presentation/screens/add_measurement_screen.dart
+++ frontend/lib/features/growth/presentation/screens/add_measurement_screen.dart
@@ -1,3 +1,17 @@
 import 'package:flutter/material.dart';
 import '../../controllers/growth_controller.dart';
 import '../../components/common/bottom_nav.dart';
+
+class AddMeasurementScreen extends StatefulWidget {
+  final String childId;
+  final String childName;
+  final String gender;
+  final DateTime dateOfBirth;
+
+  const AddMeasurementScreen({
+    super.key,
+    required this.childId,
+    required this.childName,
+    required this.gender,
+    required this.dateOfBirth,
+  });
