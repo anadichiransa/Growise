e41675acf35d661d50050d44548ea0f75a87643c
Anadi Chiransa <anadipiyamini@gmail.com>
2026-03-09 22:10:05 +0530

    Create the statefulWidget Skeleton

--- frontend/lib/features/auth/presentation/screens/signup_screen.dart
+++ frontend/lib/features/auth/presentation/screens/signup_screen.dart
@@ -1,1 +1,25 @@
+import 'package:flutter/material.dart';
 
+class SignUpScreen extends StatefulWidget {
+  @override
+  _SignUpScreenState createState() => _SignUpScreenState();
+}
+
+class _SignUpScreenState extends State<SignUpScreen> {
+  final _formKey = GlobalKey<FormState>();
+  bool _obscurePassword = true;
+  bool _obscureConfirmPassword = true;
+
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _confirmPasswordController =
+      TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFF2D1B41),
+      body: Container(),
+    );
+  }
+}
