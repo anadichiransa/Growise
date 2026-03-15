c74615fe3ee3b1ebabc97cf737657a14dd7b3b40
Anadi Chiransa <anadipiyamini@gmail.com>
2026-03-15 12:18:12 +0530

    Updated main.dart to fix errors- import connectivity_service.dart

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -5,13 +5,14 @@ import 'firebase_options.dart';
 import 'core/config/firebase_config.dart';
 import 'features/auth/presentation/screens/welcome_page.dart';
 import 'core/config/routes.dart';
+import 'shared/services/connectivity_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-
+  await FirebaseConfig.enableFirestoreOffline();
   await Get.putAsync(() => ConnectivityService().init());
   runApp(const GrowiseApp());
 }
@@ -24,13 +25,12 @@ class GrowiseApp extends StatelessWidget {
     return GetMaterialApp(
       title: 'Growise',
       debugShowCheckedModeBanner: false,
-      initialRoute: AppRoutes.welcome, // ← ADD
-      getPages: AppRoutes.pages, // ← ADD
+      initialRoute: AppRoutes.welcome,
+      getPages: AppRoutes.pages,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF4CAF50)),
         useMaterial3: true,
       ),
-      home: const OnboradingScreen(),
     );
   }
 }
