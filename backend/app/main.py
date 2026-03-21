fa6365901ce7c773ab7abaef0359f4e6d6980178
AsindU_YasitH <asinduyasithudara.2004@gmail.com>
2026-03-21 22:24:06 +0530

    error fixing

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,20 +1,25 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.api.v1 import children  
+
+from app.api.v1 import children
 from app.api.v1 import vaccines
+
 app = FastAPI(title="Growise API")
 
-
+# CORS (allow frontend connection)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["*"],   # later you can restrict to your frontend URL
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
+# register routes
 app.include_router(children.router, prefix="/api/v1")
+app.include_router(vaccines.router, prefix="/api/v1")
 
+# test route
 @app.get("/")
 def read_root():
     return {"message": "Growise API is running"}
