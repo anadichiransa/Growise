2b05a300aea6834d003ec0f135ee8f38275e3928
Anadi Chiransa <anadipiyamini@gmail.com>
2026-03-28 10:20:22 +0530

    Fixes

--- frontend/lib/core/config/api_config.dart
+++ frontend/lib/core/config/api_config.dart
@@ -1,8 +1,8 @@
-import 'dart:io';
+import 'package:flutter/foundation.dart';
 
 class ApiConfig {
   static String get baseUrl {
-    if (Platform.isAndroid) {
+    if (!kIsWeb && defaultTargetPlatform == TargetPlatform.android) {
       return "http://10.0.2.2:8000/api/v1";
     }
     return "http://127.0.0.1:8000/api/v1";
